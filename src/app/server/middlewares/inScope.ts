9235b9c9c1405016fbbcf06843e045a5218d0218
Olavo <olavo.santos@skipthedishes.ca>
2021-02-06 12:48:19 -0600

    removed unnecessary null checks

--- src/app/server/middlewares/inScope.ts
+++ src/app/server/middlewares/inScope.ts
@@ -5,15 +5,15 @@ import type { RequestHandler } from 'express';
 export const inScope: RequestHandler = (req, res, next) => {
   const scope = getNamespace('request');
   return scope?.run(() => {
-    const container = app.container?.clone();
-    container?.value('request', req);
-    container?.value('response', res);
+    const container = app.container.clone();
+    container.value('request', req);
+    container.value('response', res);
 
     scope.set('use', (namespace: string) => {
       try {
-        return container?.use(namespace);
+        return container.use(namespace);
       } catch (e) {
-        return app.container?.use(namespace);
+        return app.container.use(namespace);
       }
     });
 
