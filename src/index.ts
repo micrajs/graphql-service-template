da9336da8983633006083f69cf41c05ac32add0f
Olavo <olavo.santos@skipthedishes.ca>
2021-02-21 10:40:59 -0600

    added comment about keepAliveTimeout

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import app from '@micra/application';
 
 export const handler = app.run();
 
+/** Ref.: https://adamcrowder.net/posts/node-express-api-and-aws-alb-502 */
 handler.keepAliveTimeout =
   Number(process.env.PORT) + Number(process.env.KEEP_ALIVE_TIMEOUT);
 
